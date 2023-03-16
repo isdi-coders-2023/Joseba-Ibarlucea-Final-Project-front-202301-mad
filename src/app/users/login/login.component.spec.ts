2f55bd0dd89d7d7dab673877952f07f6bb68c2e4
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-16 20:17:56 +0100

    Add login tests

--- src/app/users/login/login.component.spec.ts
+++ src/app/users/login/login.component.spec.ts
@@ -1,23 +1,64 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
+describe('Given the LoginComponent', () => {
   let component: LoginComponent;
+  let service: UserService;
   let fixture: ComponentFixture<LoginComponent>;
+  const mockUserService = {
+    registerUser: () => {},
+    logUser: new Observable(),
+    token$: new BehaviorSubject({ results: { token: 'test' } }),
+  };
+
+  const mockResp = {
+    results: {
+      token: '',
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
+
+      declarations: [LoginComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
+    service = TestBed.inject(UserService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When the component is rendered', () => {
+    it('Then email field should be empty', () => {
+      let email = component.login.controls['email'];
+      expect(email.valid).toBeFalsy();
+    });
+  });
+
+  describe('When the handleSubmit method is called', () => {
+    it('Then should submit the login info', () => {
+      const handle = spyOn(component, 'handleSubmit').and.callThrough();
+      const token = spyOn(service, 'logUser').and.returnValue(of(mockResp));
+      component.handleSubmit();
+      expect(token).toHaveBeenCalled();
+      expect(handle).toHaveBeenCalled();
+    });
   });
 });
