11d8601a6dce5d49c5277281f240e32add2f054c
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-25 22:15:28 +0100

    Add rankings service tests

--- src/app/services/rankings/rankings.service.spec.ts
+++ src/app/services/rankings/rankings.service.spec.ts
@@ -1,16 +1,37 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Ranking } from 'src/types/types';
 
 import { RankingsService } from './rankings.service';
 
 describe('RankingsService', () => {
   let service: RankingsService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(RankingsService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should bring the array rankings when queryRanking is called', () => {
+    service.queryRanking().subscribe((res) => {
+      expect(res).toEqual([{} as Ranking]);
+    });
+
+    const req = httpController.expectOne('http://localhost:4321/rankings');
+    expect(req.request.method).toEqual('GET');
+    req.flush({ results: [{}] });
+
+    httpController.verify();
   });
 });
