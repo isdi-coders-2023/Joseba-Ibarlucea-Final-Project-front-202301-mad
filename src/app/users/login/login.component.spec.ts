bbbc0f3e1992bc128c4b3e06d8df6218cf4e2a90
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-26 21:02:46 +0200

    Add login navigation tests

--- src/app/users/login/login.component.spec.ts
+++ src/app/users/login/login.component.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
 import { UserService } from 'src/app/services/users/user.service';
+import { User } from 'src/types/types';
 
 import { LoginComponent } from './login.component';
 
@@ -60,5 +61,17 @@ describe('Given the LoginComponent', () => {
       expect(token).toHaveBeenCalled();
       expect(handle).toHaveBeenCalled();
     });
+  });
+
+  it('should handle login error', () => {
+    const logError = spyOn(service, 'logUser').and.returnValue(
+      throwError('Invalid credentials')
+    );
+    const errorMessage = 'Error on login, check your credentials';
+
+    component.handleSubmit();
+
+    expect(logError).toHaveBeenCalled();
+    expect(component.errorMessage).toBe(errorMessage);
   });
 });
