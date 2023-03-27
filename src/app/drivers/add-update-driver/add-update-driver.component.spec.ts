2e081adbb33e0cd81f19e0cbafc77a18c67188c1
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-28 00:25:07 +0200

    Add update-add driver test base

--- src/app/drivers/add-update-driver/add-update-driver.component.spec.ts
+++ src/app/drivers/add-update-driver/add-update-driver.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { DriversService } from 'src/app/services/drivers/drivers.service';
 
 import { AddUpdateDriverComponent } from './add-update-driver.component';
 
@@ -6,11 +12,27 @@ describe('AddUpdateDriverComponent', () => {
   let component: AddUpdateDriverComponent;
   let fixture: ComponentFixture<AddUpdateDriverComponent>;
 
+  const mockRoute = {
+    params: of({ id: '1324' }),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddUpdateDriverComponent ]
-    })
-    .compileComponents();
+      declarations: [AddUpdateDriverComponent],
+      providers: [
+        DriversService,
+        {
+          provide: ActivatedRoute,
+          useValue: mockRoute,
+        },
+      ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddUpdateDriverComponent);
     component = fixture.componentInstance;
@@ -19,5 +41,9 @@ describe('AddUpdateDriverComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Given the addDriver method', () => {
+    it('Should take the form values and create a new driver', () => {});
   });
 });
